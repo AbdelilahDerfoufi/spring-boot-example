beef9eabf65bae0457c149d4a4106d9f92e7bd45
Der-ab <derfoufi1996@gmail.com>
2022-12-10 12:25:21 +0100

    First push

---
+++ src/test/java/com/derAb/springbootexample/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derAb.springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
