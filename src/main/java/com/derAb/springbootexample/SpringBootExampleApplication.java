96b73fd292831db0812f2c5d51b94b7cf832491c
Der-ab <derfoufi1996@gmail.com>
2022-12-11 01:12:12 +0100

    Use a class instead of a record

--- src/main/java/com/derAb/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/derAb/springbootexample/SpringBootExampleApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class SpringBootExampleApplication {
@@ -14,11 +16,42 @@ public class SpringBootExampleApplication {
 	}
 
 	@GetMapping("/greet")
-	public GreetResponse greeting()
-	{
+	public GreetResponse greeting(){
 		return new GreetResponse("Hello Everyone");
 	}
 
-	record GreetResponse(String greet){}
+	// record GreetResponse(String greet){}
+
+	class GreetResponse {
+		private final String greet;
+
+		GreetResponse(String greet) {
+			this.greet = greet;
+		}
+
+		public String getGreet() {
+			return greet;
+		}
+
+		@Override
+		public String toString() {
+			return "GreetResponse{" +
+					"greet='" + greet + '\'' +
+					'}';
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			GreetResponse that = (GreetResponse) o;
+			return Objects.equals(greet, that.greet);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(greet);
+		}
+	}
 
 }
