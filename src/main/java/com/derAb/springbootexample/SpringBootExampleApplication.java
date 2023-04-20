041f9f22b4059aa9dd2ff7c19bac5fd9083fc1e3
Der-ab <derfoufi1996@gmail.com>
2023-04-20 22:44:29 +0000

    uri change

--- src/main/java/com/derAb/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/derAb/springbootexample/SpringBootExampleApplication.java
@@ -3,28 +3,30 @@ package com.derAb.springbootexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/api/v1/customer")
 public class SpringBootExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
-
-	@GetMapping("/greet")
-	public GreetResponse greeting(){
-		return new GreetResponse("Hello Everyone",List.of("C++","Php","Java"), new Person("Der Ab", 26, 1000));
-	}
-
-	record Person(String name, int age, double savings){}
-
-	record GreetResponse(String greet,
-						 List<String> favProgrammingLanguages,
-						 Person person){}
+//
+//	@GetMapping("/greet")
+//	public GreetResponse greeting(){
+//		return new GreetResponse("Hello Everyone",List.of("C++","Php","Java"), new Person("Der Ab", 26, 1000));
+//	}
+//
+//	record Person(String name, int age, double savings){}
+//
+//	record GreetResponse(String greet,
+//						 List<String> favProgrammingLanguages,
+//						 Person person){}
 
 //	class GreetResponse {
 //		private final String greet;
@@ -58,4 +60,8 @@ public class SpringBootExampleApplication {
 //		}
 //	}
 
+	@GetMapping
+	public List<Customer> getCustomer() {
+		return List.of();
+	}
 }
